edfe7705e08e41c70efbdd85ef2f64b7d8311de2
RHillenbrand25 <S01141102@acad.tri-c.edu>
2019-02-10 15:49:29 -0500

    Enrollment Application 2

---
+++ EnrollmentApplication2/EnrollmentApplication2/Controllers/Enrollment2Controller.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EnrollmentApplication2.Models;
+
+namespace EnrollmentApplication2.Controllers
+{
+    public class Enrollment2Controller : Controller
+    {
+        private Enrollment2DB db = new Enrollment2DB();
+
+        // GET: Enrollment2
+        public ActionResult Index()
+        {
+            var enrollments = db.Enrollments.Include(e => e.StudentObject);
+            return View(enrollments.ToList());
+        }
+
+        // GET: Enrollment2/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollment2/Create
+        public ActionResult Create()
+        {
+            ViewBag.StudentId = new SelectList(db.Students, "StudentId", "LastName");
+            return View();
+        }
+
+        // POST: Enrollment2/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EnrollmentId,StudentId,CourseId,Grade,CourseObject")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrollments.Add(enrollment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StudentId = new SelectList(db.Students, "StudentId", "LastName", enrollment.StudentId);
+            return View(enrollment);
+        }
+
+        // GET: Enrollment2/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StudentId = new SelectList(db.Students, "StudentId", "LastName", enrollment.StudentId);
+            return View(enrollment);
+        }
+
+        // POST: Enrollment2/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EnrollmentId,StudentId,CourseId,Grade,CourseObject")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StudentId = new SelectList(db.Students, "StudentId", "LastName", enrollment.StudentId);
+            return View(enrollment);
+        }
+
+        // GET: Enrollment2/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollment2/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = db.Enrollments.Find(id);
+            db.Enrollments.Remove(enrollment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EnrollmentApplication2/EnrollmentApplication2/Models/Enrollment2DB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EnrollmentApplication2.Models
+{
+    public class Enrollment2DB : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Enrollment2DB() : base("name=Enrollment2DB")
+        {
+        }
+
+		public System.Data.Entity.DbSet<EnrollmentApplication2.Models.Enrollment> Enrollments { get; set; }
+
+		public System.Data.Entity.DbSet<EnrollmentApplication2.Models.Student> Students { get; set; }
+
+		public System.Data.Entity.DbSet<EnrollmentApplication2.Models.Course> Courses { get; set; }
+	}
+}
