7b1c756513168db88b8c63b6e3105e7206db1b4a
RHillenbrand25 <S01141102@acad.tri-c.edu>
2019-03-03 14:53:45 -0500

    Lab 7 Part 2

--- EnrollmentApplication2/EnrollmentApplication2/Models/Student.cs
+++ EnrollmentApplication2/EnrollmentApplication2/Models/Student.cs
@@ -18,5 +18,36 @@ namespace EnrollmentApplication2.Models
 		[Required(ErrorMessage = "First Name")]
 		[StringLength(50)]
 		public virtual string FirstName { get; set; }
+
+		public string Address1 { get; set; }
+
+		[Compare(nameof(Address1), ErrorMessage = "Address2 cannot be the same as Address1")]
+		public string Address2 { get; set; }
+
+		public string City { get; set; }
+
+		[StringLength(5, ErrorMessage = "Enter a 5 digit Zipcode")]
+		public string Zipcode { get; set; }
+
+		[StringLength(2, ErrorMessage = "Enter a 2 digit State code")]
+		public string State { get; set; }
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (Address2 = Address1)
+			{
+				yield return (new ValidationResult("Address2 cannot be the same as Address1"));
+			}
+			if (Zipcode = 5)
+			{
+				yield return (new ValidationResult("Zipcode must be a 5 digit Zipcode"));
+			}
+			if (State = 2)
+			{
+				yield return (new ValidationResult("State must be a 2 digit State code"));
+			}
+
+			throw new NotImplementedException();
+		}	
 	}
 }
