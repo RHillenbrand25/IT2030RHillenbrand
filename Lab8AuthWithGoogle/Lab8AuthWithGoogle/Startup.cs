a560461d2261865499ba7b9c84f009df81c8f73a
RHillenbrand25 <S01141102@acad.tri-c.edu>
2019-03-07 16:39:38 -0500

    Lab8AuthWithGoogle

---
+++ Lab8AuthWithGoogle/Lab8AuthWithGoogle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab8AuthWithGoogle.Startup))]
+namespace Lab8AuthWithGoogle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
